9ae2f889ead4a1b69ca12143cc3945635dbd2e80
BurakKurt <krtburak@outlook.com>
2023-06-21 23:13:34 +0300

    project created

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <>
+            <p className="text-danger">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</p>
+        </>
+    )
+}
+
+export default App
